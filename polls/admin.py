acc0c883cb43ca46c10fcae0cd652bb0061d5872
tutorua <tutorua@ukr.net>
2017-11-08 14:30:12 +0200

    Introduced form fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,9 @@ from .models import Question
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
-    
+    fieldsets = [
+        ('Question group title', {'fields': ['question_text']}),
+        ('Date information',     {'fields': ['pub_date']}),
+        ]
+
 admin.site.register(Question, QuestionAdmin)
